f1c4d168c28600c0efb07ab181b4cc1c811d97d4
alexandrebaccega <33407477+alexandrebaccega@users.noreply.github.com>
2023-04-17 19:37:14 -0300

    Ajstem4

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
-import os
+from flask_wtf.csrf import CSRFProtect
+
 app = Flask(__name__)
+
+csrf = CSRFProtect(app)                                                                                                                           
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Laboratório Pipeline DevOps"
 
 if __name__ == '__main__':
-    port = os.getenv('PORT')
-    app.run('0.0.0.0', port=port)
+    app.run()
